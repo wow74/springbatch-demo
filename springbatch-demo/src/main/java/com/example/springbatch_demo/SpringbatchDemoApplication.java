7e76d18454e746dd8c6bdda0222edcc74d583b15
info1 <info.mailaddress1@gmail.com>
2024-12-10 09:59:36 +0900

    バッチセットアップ

---
+++ springbatch-demo/src/main/java/com/example/springbatch_demo/SpringbatchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatch_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchDemoApplication.class, args);
+	}
+
+}
