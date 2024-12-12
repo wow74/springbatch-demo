bd963d3b821d67f6a2a99838a26b37138a4b4fb1
info1 <info.mailaddress1@gmail.com>
2024-12-12 18:37:17 +0900

    job, stepのExecutionContextの実装

--- HelloWorld/tasklet/demo/src/main/java/com/example/demo/config/BatchConfig.java
+++ HelloWorld/tasklet/demo/src/main/java/com/example/demo/config/BatchConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import com.example.demo.tasklet.HelloTasklet;
+import com.example.demo.tasklet.HelloTasklet2;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
@@ -8,6 +9,7 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -16,7 +18,12 @@ import org.springframework.transaction.PlatformTransactionManager;
 public class BatchConfig {
 
   @Autowired
+  @Qualifier("HelloTasklet")
   private HelloTasklet helloTasklet;
+
+  @Autowired
+  @Qualifier("HelloTasklet2")
+  private HelloTasklet2 helloTasklet2;
 
   // spring3系
   @Bean
@@ -26,12 +33,20 @@ public class BatchConfig {
             .build(); // Stepの生成
   }
 
+  @Bean
+  public Step sampleStep2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    return new StepBuilder("HelloStep2", jobRepository)
+            .tasklet(helloTasklet2, transactionManager)
+            .build();
+  }
+
   // spring3系
   @Bean
   public Job sampleJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) throws Exception {
     return new JobBuilder("HelloJob", jobRepository)
             .incrementer(new RunIdIncrementer()) // IDのインクリメント
             .start(sampleStep(jobRepository, transactionManager)) // 最初のstep
+            .next(sampleStep2(jobRepository, transactionManager))
             .build();  // Jobの生成
   }
 }
--- HelloWorld/tasklet/demo/src/main/java/com/example/demo/tasklet/HelloTasklet.java
+++ HelloWorld/tasklet/demo/src/main/java/com/example/demo/tasklet/HelloTasklet.java
@@ -4,16 +4,33 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
-@Component
+import java.util.concurrent.ExecutionException;
+
+@Component("HelloTasklet")
 @StepScope
 public class HelloTasklet implements Tasklet {
 
   @Override
   public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
     System.out.println("Hello World");
+
+    ExecutionContext jobContext = contribution
+            .getStepExecution()
+            .getJobExecution()
+            .getExecutionContext(); // JobExecutionContext(別stepに値を渡せる)
+
+    jobContext.put("jobKey", "jobValue");
+
+    ExecutionContext stepContext = contribution
+            .getStepExecution()
+            .getExecutionContext(); // StepExecutionContext
+
+    stepContext.put("stepKey", "stepValue");
+
     return RepeatStatus.FINISHED;
   }
 }
