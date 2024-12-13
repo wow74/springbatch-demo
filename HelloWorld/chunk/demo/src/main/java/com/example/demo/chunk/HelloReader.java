13bc43b01bffbda9164674b687bf118310997a2e
info1 <info.mailaddress1@gmail.com>
2024-12-13 11:48:48 +0900

    job step間での変数の受け渡し実装

--- HelloWorld/chunk/demo/src/main/java/com/example/demo/chunk/HelloReader.java
+++ HelloWorld/chunk/demo/src/main/java/com/example/demo/chunk/HelloReader.java
@@ -1,10 +1,9 @@
 package com.example.demo.chunk;
 
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.NonTransientResourceException;
-import org.springframework.batch.item.ParseException;
-import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.batch.item.*;
 import org.springframework.stereotype.Component;
 
 
@@ -15,6 +14,20 @@ public class HelloReader implements ItemReader<String> {
   // nullが返ると処理が終了となる
   private String[] input = {"Hello", "World", null, "No Read"};
   private int index = 0;
+
+  @BeforeStep
+  public void beforeStep(StepExecution stepExecution) {
+    ExecutionContext jobContext = stepExecution
+            .getJobExecution()
+            .getExecutionContext();
+
+    jobContext.put("jobKey", "jobValue");
+
+    ExecutionContext stepContest = stepExecution
+            .getExecutionContext();
+
+    stepContest.put("stepKey", "stepValue");
+  }
 
   @Override
   public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
